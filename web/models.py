a517ad569a8f84f4ffcb2308aa02d6badaef1b09
duanlingxiao <duanlingxiao@jd.com>
2018-07-10 02:28:27 +0800

    secod

--- web/models.py
+++ web/models.py
@@ -1,6 +1,49 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
 
 # Create your models here.
+@python_2_unicode_compatible
+class Article(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255, null=True)
+    content = models.CharField(max_length=4096, null=True)
+    comment = models.CharField(max_length=1024, null=True)
+    like = models.IntegerField(default=0, null=True)
+    created_at = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = "article"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def image(self):
+        return Image.objects.get(article_id_id = self.id)
+
+@python_2_unicode_compatible
+class Image(models.Model):
+    id = models.IntegerField(primary_key=True)
+    article_id = models.IntegerField( null=True)
+    product_id = models.IntegerField( null=True)
+    file_name = models.CharField(max_length=255, null=True)
+    article_id = models.ForeignKey(Article, db_column='article_id')
+
+    class Meta:
+        db_table = "image"
+
+    def __str__(self):
+        return self.file_name
+
+# class Product(models.Model):
+    
+
+#     def __str__(self):
+#         return 
+
+#     def __unicode__(self):
+#         return 
--- web/views.py
+++ web/views.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
+#encoding:utf8
 from __future__ import unicode_literals
 
 from django.shortcuts import render, render_to_response
-
+from models import Article
+from django.conf import settings
 # Create your views here.
 
 
@@ -10,19 +11,28 @@ def index(request):
     return render_to_response("index.html", locals())
 
 def aboutus(request):
-    return render_to_response("index.html", locals())
+    return render_to_response("aboutus.html", locals())
 
 def contact(request):
-    return render_to_response("index.html", locals())
+    return render_to_response("contact.html", locals())
 
 def joinus(request):
-    return render_to_response("index.html", locals())
+    return render_to_response("joinus.html", locals())
 
 def news_detail(request):
-    return render_to_response("index.html", locals())
+    article_id =  int(request.GET.get("article_id"))
+    article = Article.objects.get(id=article_id)
+    return render_to_response("news_detail.html", locals())
+
+def get_short_content(value):
+    value.content = value.content[:60] + '...'
+    IMAGE_API_HOST = settings.IMAGE_HOST
+    value.src = IMAGE_API_HOST + value.image.file_name
+    return value
 
 def news(request):
-    return render_to_response("index.html", locals())
+    news_list = map(get_short_content, Article.objects.all().order_by('-id'))
+    return render_to_response("news.html", locals())
 
 def product(request):
-    return render_to_response("index.html", locals())
+    return render_to_response("product.html", locals())
